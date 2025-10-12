abcc313339cb4bc2af880119bd30473875161737
Sanaunggithub <usasanaung2000@gmail.com>
2025-10-12 13:22:18 +0700

    added arrayPairSum.py

---
+++ Array/Easy/arrayPairSum.py
@@ -0,0 +1,11 @@
+def arrayPairSum(nums):
+    nums.sort()
+
+    res = 0
+    for i in range(0, len(nums), 2):
+        res += nums[i]
+
+    return res
+    
+nums = [1,4,3,2]
+print(arrayPairSum(nums))
