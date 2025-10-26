7fca39488185b69a5aeaa3fc1f626904953a0bd6
Sanaunggithub <usasanaung2000@gmail.com>
2025-10-26 19:19:55 +0700

    added firstPalindrome.py

---
+++ String/firstPalindrome.py
@@ -0,0 +1,10 @@
+def firstPalindrome(words):
+    for word in words:
+        if word == word[::-1]:
+            return word
+
+
+    return ""
+
+words = ["abc","car","ada","racecar","cool"]
+print(firstPalindrome(words))
