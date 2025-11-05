05ad5bc6a908fcf1cb7f0440ca035dbf50ccf006
Sanaunggithub <usasanaung2000@gmail.com>
2025-11-05 21:55:52 +0700

    added solveNQueens.py

---
+++ Backtracking/solveNQueens.py
@@ -0,0 +1,33 @@
+def solveNQueens(self, n):
+    col = set()
+    posDiag = set() # r + c
+    negDiag = set() #r - c
+
+    res = []
+
+    board = [["."] * n for _ in range(n)]
+
+    def backtrack(r):
+        if r == n:
+            copy = ["".join(row) for row in board]
+            res.append(copy)
+            return 
+
+        for c in range(n):
+            if c in col or r + c in negDiag or r - c in posDiag:
+                continue
+
+            col.add(c)
+            negDiag.add(r+c)
+            posDiag.add(r-c)
+            board[r][c] = "Q"
+
+            backtrack(r+1)
+
+            col.remove(c)
+            negDiag.remove(r+c)
+            posDiag.remove(r-c)
+            board[r][c] = "."
+    
+    backtrack(0)
+    return res
