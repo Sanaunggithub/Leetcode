aeb5ae94ba7c86f1bebd2ded9b8fb9be0b52d314
Sanaunggithub <usasanaung2000@gmail.com>
2025-11-06 12:27:31 +0700

    added jumpGame.py

--- Array/Medium/jumpGame.py
+++ Array/Medium/jumpGame.py
@@ -1,18 +1,11 @@
 def canJump(nums):
-    last = len(nums) - 1
+    goal = len(nums) - 1
+
+    for i in range(len(nums) -1, -1, -1):
+        if i + nums[i] >= goal:
+            goal = i
     
-    max_reachable = 0
-
-    for i in range(len(nums)):
-        if i > max_reachable:
-            return False
-        
-        max_reachable = max(max_reachable, i + nums[i])
-
-        if max_reachable >= last:
-            return True
-        
-    return False
+    return True if goal == 0 else False
 
         
         
