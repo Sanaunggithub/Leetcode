ffb82cd8fc04e45ba3dbc1ed93d19e3b26fe84e1
Sanaunggithub <usasanaung2000@gmail.com>
2025-11-07 14:48:04 +0700

    added spiralMatrix.py

---
+++ Array/Medium/spiralMatrix.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        
+        res = []
+
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+
+        while top < bottom and left < right:
+            # get every i in top row
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            top += 1
+
+            # get every i in right col
+            for i in range(top, bottom):
+                res.append(matrix[i][right - 1])
+            right -= 1
+
+            if not (top < bottom and left < right):
+                break
+
+            # get every i in bottom row:
+            for i in range(right -1, left -1 , -1):
+                res.append(matrix[bottom -1][i])
+            bottom -= 1
+            
+            # get every i in left col:
+            for i in range(bottom -1 , top -1, -1):
+                res.append(matrix[i][left])
+            left += 1
+
+        return res
