24b48112ec4b3c53abd29ed1a84ad7ac3616eab8
Sanaunggithub <usasanaung2000@gmail.com>
2025-12-05 22:33:36 +0700

    added BalancedTree.java

---
+++ Tree/BalancedTree.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) != -1;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int left = dfs(root.left);
+        if (left == -1)
+            return -1; // prune
+
+        int right = dfs(root.right);
+        if (right == -1)
+            return -1; // prune
+
+        if (Math.abs(left - right) > 1)
+            return -1; // unbalanced
+
+        return 1 + Math.max(left, right);
+    }
+}
