4e4ebf6c6aeaffbd1fd3223c21e0ccc4662150f9
Sanaunggithub <usasanaung2000@gmail.com>
2026-01-17 01:48:12 +0700

    added peakElement.py

--- Binary Search/Medium/peakElement.py
+++ Binary Search/Medium/peakElement.py
@@ -5,8 +5,8 @@ def findPeakElement(self, nums):
 
         mid = (l + h) // 2
 
-        left = nums[mid - 1] if mid > 0 else float('-inf')
-        right = nums[mid + 1] if mid < len(nums) - 1 else float('-inf')
+        left = nums[mid - 1] if mid > 0 else float('-inf') # negative infinity
+        right = nums[mid + 1] if mid < len(nums) - 1 else float('-inf') # positive infinity
 
         if left < nums[mid] and nums[mid] > right:
             return mid
